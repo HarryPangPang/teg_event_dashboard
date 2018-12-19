8f4899bc7051ed83bbdc604fdf20c437dad3c9dc
“gitZhangQiangQQ <1562055324@qq.com>
2018-12-19 15:48:26 +0800

    add filter phone

--- event_dashboard_web/src/components/HelloWorld.vue
+++ event_dashboard_web/src/components/HelloWorld.vue
@@ -1,100 +1,96 @@
-<template>
-<el-row :gutter="20">
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-</el-row>
+/* <template>
+  <el-row :gutter="20">
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-import eventApi from '../api/api.js'
+import eventApi from "../api/api.js";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   },
-  data(){
-    return{
-      allInfo:''
-    }
+  data() {
+    return {};
   },
   created() {
-    this.getAll()
+    this.getAllData();
   },
-  methods:{
-    getAll(){
-      // return new Promise(()=>{
-        this.$axios
-        .get(eventApi.getAllUsers)
-        .then(response => (
-          // this.allInfo = response.data
-          // console.log(response.data[0])
-        )=>{
-return (response.data[0])
-        })
-      // })
-      },
-     getlog(){
-      return new Promise((res)=>{
-        console.log('log')
-      })
+  methods: {
+    getAll() {
+      return new Promise((resolve, reject) => {
+        this.$axios.get(eventApi.getAllUsers).then(response => {
+          let allConsumer = response.data;
+          // console.log(allConsumer);
+          resolve(allConsumer);
+        });
+      });
     },
-     getAllData: async function () {
-      await this.getlog();
-        await console.log('aaa')
-        await this.getAll();
-        
-      } ,
-      test: async  () => {
-        var a = function(){
-      return new Promise((res)=>{
-        console.log('log')
-      })
-        };
-
-        var b = function(){
-          axios
-        .get(eventApi.getAllUsers)
-        .then(response => (
-          // this.allInfo = response.data
-          console.log(response.data[0])
-        ))
-        }
-        try {
-          await b();
-          await a();
-        } catch (error) {
-          console.log(error)
-        }
+    checkPhone(usefulPhone) {
+      if (/^1[34578]\d{9}$/.test(usefulPhone)) {
+        // console.log("OK");
+        return 1
+      } else {
+        // console.log("手机号有误");
+        return 0
       }
+    },
+    filterUsefulMobile(telArr) {
+      var usefulMobileConsumer = [];
+       var unUsefulMobileConsumer = [];
+      telArr.forEach(item => {
+        if(this.checkPhone(item.contact_info1) == 1){
+          usefulMobileConsumer.push(item)
+        } else{
+          unUsefulMobileConsumer.push(item)
+        }
+      });
+      console.log(unUsefulMobileConsumer)
+    },
+    async getAllData() {
+      let telArr = await this.getAll();
+      await this.filterUsefulMobile(telArr);
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .el-row {
-    margin-bottom: 20px;
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
+ */
