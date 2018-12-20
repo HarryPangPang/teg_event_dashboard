5b8939ff5e690f6c08793824b845be30f52dc1a9
“gitZhangQiangQQ <1562055324@qq.com>
2018-12-20 17:43:12 +0800

    新

--- event_dashboard_web/src/api/api.js
+++ event_dashboard_web/src/api/api.js
@@ -1,5 +1,5 @@
 var eventApi = {
-    getAllUsers: 'http://localhost:3000/apiConsumer/getAll',
+    getAllUsers: 'http://localhost:3000/apiConsumer/getAllgetAllConsumer',
     searchConsumerEvent:'http://localhost:3000/apiConsumer/searchConsumerEvent',
     searchConsumerInfoByPhone:'http://localhost:3000/apiConsumer/searchConsumerInfoByPhone'
 }
