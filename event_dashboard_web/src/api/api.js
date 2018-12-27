fd7258eec8ea446eb42d571c3974b6e16aef71dc
“gitZhangQiangQQ <1562055324@qq.com>
2018-12-27 16:11:34 +0800

    修改export

--- event_dashboard_web/src/api/api.js
+++ event_dashboard_web/src/api/api.js
@@ -2,7 +2,9 @@ var eventApi = {
     getAllUsers: 'http://localhost:3000/apiConsumer/getAllgetAllConsumer',
     getAllEvent: 'http://localhost:3000/apiConsumer/getAllEvent',
     searchConsumerEvent:'http://localhost:3000/apiConsumer/searchConsumerEvent',
-    searchConsumerInfoByPhone:'http://localhost:3000/apiConsumer/searchConsumerInfoByPhone'
+    searchConsumerInfoByPhone:'http://localhost:3000/apiConsumer/searchConsumerInfoByPhone',
+    getConsumerCameWhichEvent:'http://localhost:3000/apiConsumer/getEvenrCamers',
+    getConsumerSCameWhichEvent:'http://localhost:3000/apiConsumer/getAllEventMemberNum'
 }
 
-module.exports = eventApi
+export default eventApi
